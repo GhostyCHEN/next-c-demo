87e28e73a767c2dd4a045d0ac97f019738f0e466
donggenmu <chenyinjiewj@gmail.com>
2021-01-20 22:50:24 +0800

    路由-跳转时用query传递和接受参数

--- pages/expB.js
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link'
-
-export default () => (
-    <>
-        <div>Next - B</div>
-        <Link href='/'><a>返回首页</a></Link>
-    </>
-)
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,32 @@ import React from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 
+
 const Home = () => {
-  function gotoA(){
-    Router.push('/expA')
+
+  function choose(){
+    // Router.push('/desc?name=B')
+    // 面向对象传递
+    Router.push({
+      pathname:'/desc',
+      query:{
+        name:'C'
+      }
+    })
   }
 
   return (
     <>
-    <div>我是首页</div>
-    <div><Link href="/expA"><a>去A页面</a></Link></div>
-    <div><Link href="/expB"><a>去B页面</a></Link></div>
-    <div>
-      <button onClick={gotoA}>去A页面</button>
-    </div>
-  </>
+      <div>我是首页</div>
+      <div><Link href="/desc?name=A"><a>选择A</a></Link></div>
+      <div><Link href="/desc?name=B"><a>选择B</a></Link></div>
+      <div><Link href={{pathname:'/desc',query:{name:'D'}}}><a>选择D</a></Link></div>
+      <div>
+        <button onClick={choose}>
+          选择C
+        </button>
+      </div>
+    </>
   )
 }
 
