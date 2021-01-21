128ea65d7b937e4b2d381cea685d7c10a7e388b3
donggenmu <chenyinjiewj@gmail.com>
2021-01-21 23:01:14 +0800

    路由的6个钩子事件

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,40 @@ import Router from 'next/router'
 
 const Home = () => {
 
+  // routeChangeStart
+  // routeChangeComplete
+  // beforeHistoryChange
+  // routeChangeError
+  // hashChangeStart
+  // hashChangeComplete
+
+  Router.events.on('routeChangeStart',(...args) => {
+    console.log('1.routeChangeStart -> 路由开始变化，参数为：',...args);
+  })
+
+  Router.events.on('routeChangeComplete',(...args) => {
+    console.log('2.routeChangeComplete -> 路由变化结束，参数为：',...args);
+  })
+
+  Router.events.on('beforeHistoryChange',(...args) => {
+    console.log('3.beforeHistoryChange -> 路由变化结束，参数为：',...args);
+  })
+
+  // 不包括404错误
+  Router.events.on('routeChangeError',(...args) => {
+    console.log('4.routeChangeError -> 路由发生错误，参数为：',...args);
+  })
+
+  // hash模式
+  Router.events.on('hashChangeStart',(...args) => {
+    console.log('5.hashChangeStart -> ，参数为：',...args);
+  })
+
+  Router.events.on('hashChangeComplete',(...args) => {
+    console.log('6.hashChangeComplete -> ，参数为：',...args);
+  })
+
+
   function choose(){
     // Router.push('/desc?name=B')
     // 面向对象传递
@@ -27,6 +61,7 @@ const Home = () => {
           选择C
         </button>
       </div>
+      <div><Link href="#hashModel"><a>hash模式</a></Link></div>
     </>
   )
 }
